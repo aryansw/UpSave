78d25d2c6e8f397633a2a678849acf3160c94511
freezeroid <37911261+freezeroid@users.noreply.github.com>
2020-02-29 17:07:37 -0600

    Revert "Fixed Merge Conflict"
    This reverts commit cdc9ce9dfb0ee94e87303baaaa1326223f5fbcac, reversing
    changes made to cd3a6f1432cebcfd91a575cf79d27294a5f74391.

--- UpSave/Our_Scripts/ReadDictionary.cs
+++ UpSave/Our_Scripts/ReadDictionary.cs
@@ -12,11 +12,9 @@ using UpSave.Our_Scripts;
 
 namespace UpSave
 {
-    public class ReadCustomerDictionary
+    public class ReadDictionary
     {
-
-        public static List<string> readCustomerDictionary()
-
+        public static string ReadCustomerDictionary()
         {
             HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(string.Format("http://api.reimaginebanking.com/customers?key=2576f38896155fd18751261143cff4c4"));
             webRequest.Method = "GET";
@@ -32,45 +30,16 @@ namespace UpSave
             }
 
             System.Diagnostics.Debug.WriteLine(jsonArrayString);
-            jsonArrayString = jsonArrayString.Substring(1, jsonArrayString.Length - 1);
-            char[] charArray = new char[jsonArrayString.Length];
-            List<string> jsonStrings = new List<string>();
-            int flag = 0;
-            string eachString = "";
-            for (int i = 0; i < jsonArrayString.Length; i++)
-            {
-                char c = charArray[i];
-                if (c == '{')
-                {
-                    if (flag != 0)
-                    {
-                        charArray[i] = c;
-                    }
-                    flag++;
-                }
-                else if (c == '}')
-                {
-                    flag--;
-                    if (flag != 0)
-                    {
-                        charArray[i] = c;
-                    }
-                    for (int j = 0; j < charArray.Length; j++)
-                    {
-                        eachString += charArray[j];
-                        charArray[j] = ' ';
-                    }
-                    jsonStrings.Add(eachString);
-                    eachString = "";
-                }
-                else
-                {
-                    charArray[i] = c;
-                }
-            }
-            System.Diagnostics.Debug.WriteLine(jsonStrings);
-            return jsonStrings;
+            string jsonString = jsonArrayString.Substring(1, 197);
+            System.Diagnostics.Debug.WriteLine(jsonString);
+           
+            return jsonString;
         }
 
+        public static void Main()
+        {
+            Customer customer = new Customer(json: ReadCustomerDictionary());
+            System.Diagnostics.Debug.WriteLine(customer.Id);
+        }
     }
 }
