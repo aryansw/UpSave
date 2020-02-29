973ca311ba04ca3cb096c8d6aa904211e6ae6777
freezeroid <37911261+freezeroid@users.noreply.github.com>
2020-02-29 12:34:37 -0600

    made list work

--- UpSave/Our_Scripts/Customer.cs
+++ UpSave/Our_Scripts/Customer.cs
@@ -3,33 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Script.Serialization;
-using Newtonsoft.Json;
 
 namespace UpSave.Our_Scripts
 {
     public class Customer
     {
+        public static JavaScriptSerializer serializer = new JavaScriptSerializer();
         public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public Dictionary<string, string> Address { get; set; }
 
         public Customer(string json)
-        {   
-            Customer customer = new JavaScriptSerializer().Deserialize<Customer>(json);
-            this.LastName=customer.LastName;
-            this.FirstName=customer.FirstName;
-            this.Id=customer.Id;
-            this.Address=customer.Address;
-            System.Diagnostics.Debug.WriteLine(customer.Address);
+        {
+            Customer customer = serializer.Deserialize<Customer>(json);
+            this.LastName = customer.LastName;
+            this.FirstName = customer.FirstName;
+            this.Id = customer.Id;
+            this.Address = customer.Address;
+
         }
+        public Customer(List<string> x)
+        { int l = 0;
+            Customer a = new Customer[x.Count];
+            foreach (string i in x) 
+            { 
+                a[l] = new Customer(i);
+                l++;
+            }
+            return a;
+        }
+    
+
+        public Customer(Customer a)
+        {
+            this.LastName = customer.LastName;
+            this.FirstName = customer.FirstName;
+            this.Id = customer.Id;
+            this.Address = customer.Address;
+        }
+
         public Customer()
         {
-            this.FirstName = "broky";
+            this.FirstName = "NaNOAs";
+            this.LastName = "barrak";
             this.Id = "sid";
-
-
+            this.Address = new Dictionary<string, string>()
+            {
+                {"street_number","nil" },
+                {"street_name","potato"},
+                {"city","manhattan" },
+                {"zip","string" }
+            };
+            
         }
-
+        
     }
 }
