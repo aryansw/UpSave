ccf9f5be3792f79991e76f44471125e8fbb5858d
meghnakr <mrajago@purdue.edu>
2020-03-01 11:43:54 -0500

    Finished calculating suggestions to acheive goal.

--- UpSave/Our_Scripts/RequestHandling.cs
+++ UpSave/Our_Scripts/RequestHandling.cs
@@ -6,11 +6,12 @@ using System.Web;
 namespace UpSave.Our_Scripts
 {
     public class RequestHandling
-    {   
-        public static decimal[] ExpectedSavings(decimal[] spending, decimal income, decimal CurrentSavings, int time) 
+    {
+        public static decimal[] ExpectedSavings(decimal[] spending, decimal income, decimal CurrentSavings, int time)
         {
             decimal[] expectedSaving = new decimal[3];
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < 3; i++)
+            {
                 decimal saving = time * income - time * spending[i];
                 expectedSaving[i] = CurrentSavings + saving;
             }
@@ -22,25 +23,27 @@ namespace UpSave.Our_Scripts
         {
             decimal[] b = new decimal[a.Length];
             int count = 0;
-            foreach(float s in a)
+            foreach (float s in a)
             {
                 b[count++] = (decimal)s;
             }
             return b;
         }
-        public static decimal[] Goals(decimal[] spending, decimal[] last, float income, float CurrentSavings, float goals) {
+        public static decimal[] Goals(decimal[] spending, decimal[] last, float income, decimal CurrentSavings, float goals)
+        {
             decimal[] savings = new decimal[6];
-            for (int i = 0; i < 6; i++){
+            for (int i = 0; i < 6; i++)
+            {
                 decimal diff = spending[i] - last[i];
-                if (diff < 0) { 
-                    diff *= -1;        
+                if (diff < 0)
+                {
+                    diff *= -1;
                 }
                 savings[i] = diff;
-        }
-            
+            }
             return savings;
         }
-
-        
- }
     }
+}
+        
+ 
